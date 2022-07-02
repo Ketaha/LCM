8539d9df515800eefe42cd05fd63c30788444421
Kadir Kadir <dragonrevoke@gmail.com>
2022-07-02 19:07:16 +0300

    negElemends

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿List<int> dividers = new (Console.ReadLine().Trim().Split().Select(int.Parse));
-ICollection<int> indecies = new List<int>();
+Stack<int> indecies = new ();
 
 Arg0Exc(dividers);
 
-for (int i = 0; i < dividers.Count; i++) dividers[i] = Math.Abs(dividers[i]);
-
+if (dividers.Any(i => i < 0))
+    for (int i = 0; i < dividers.Count; i++) dividers[i] = Math.Abs(dividers[i]);
+    
 var LCM = 1;
 
 // Divider which is incramented if is too small to dividers and 
@@ -16,14 +17,13 @@ do
     // Scans sequence for numbers that are devisable to the 'div' number and stores them in a collection
     for (int i = 0; i < dividers.Count; i++) 
         if (dividers[i] % div == 0) 
-            indecies.Add(i);
+            indecies.Push(i);
 
     Div(dividers, indecies, ref LCM, ref div);
 
     // Possible 'TODO' if only one item remains just multiply it to the LCM and break the loop 
     div = div == dividers.Max() + 1 ? dividers.Min() : div;
 
-    indecies.Clear();
     // Removing all elements ends the loop
     dividers.RemoveAll(i => i == 1);
 } while (dividers.Any()); 
@@ -33,9 +33,9 @@ Console.WriteLine(LCM);
 ///<summary>
 ///If any elements are present in indecies it divides by div else increases div so division is possible
 ///</summary>
-static int Div(List<int> dividers, ICollection<int> indecies, ref int LCM, ref int div) {
+static int Div(List<int> dividers, Stack<int> indecies, ref int LCM, ref int div) {
     if (indecies.Count != 0) {
-        for (int j = 0; j < indecies.Count; j++) dividers[indecies.ElementAt(j)] /= div;
+        for (int j = 0; j < indecies.Count; j++) dividers[indecies.Pop()] /= div;
         return LCM *= div;
     }
 
